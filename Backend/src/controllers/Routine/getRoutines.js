055dcf5f407830357e03e233e5e9d8cd383cbe62
cls315 <camilasaya315@gmail.com>
2023-12-01 10:31:37 -0300

    Creacion getRoutines.js

---
+++ Backend/src/controllers/Routine/getRoutines.js
@@ -0,0 +1,36 @@
+// getRoutines.js
+
+const axios = require("axios");
+const { Routine, Trainer } = require("../../db");
+const { API_RUTINAS } = require("../urls");
+
+const getRoutines = async () => {
+  const routinesInDB = await Routine.findAll();
+
+  if (routinesInDB.length === 0) {
+    const apiRoutines = (await axios.get(API_RUTINAS)).data;
+    const mappedRoutines = apiRoutines.map((r) => ({
+      exerc: r.exerc,
+      totalDuration: r.totalDuration,
+      enfoque: r.enfoque,
+      trainerId: r.trainerId,
+      precio: r.precio,
+    }));
+
+    // Crear las rutinas en la base de datos
+    await Routine.bulkCreate(mappedRoutines);
+  }
+
+  const updatedRoutines = await Routine.findAll();
+
+  return updatedRoutines.map((routine) => ({
+    id: routine.id,
+    exerc: routine.exerc,
+    totalDuration: routine.totalDuration,
+    enfoque: routine.enfoque,
+    trainerId: routine.trainerId, // asignar el TrainerId correspondiente
+    precio: routine.precio,
+  }));
+};
+
+module.exports = getRoutines;
