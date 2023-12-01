1dbc1c3f806c42731efe2f229eaf36be7c79c466
cls315 <camilasaya315@gmail.com>
2023-12-01 18:18:05 -0300

    Creacion stripeHndls.js

---
+++ Backend/src/handlers/handlerStripe/stripeHndls.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+
+const { createCheckoutSession, statusCheckout } = require("../controllers");
+
+router.post("/create-checkout-session", async (req, res) => {
+  const data = req.body;
+  const newSession = await createCheckoutSession(data);
+
+  res.status(200).json(newSession);
+});
+
+router.get("/:session_id", async (req, res) => {
+  const { session_id } = req.params;
+  const status = await statusCheckout(session_id);
+
+  res.status(200).json(status);
+});
+
+module.exports = router;
