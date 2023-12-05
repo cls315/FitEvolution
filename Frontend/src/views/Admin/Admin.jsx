43dfcbabc4903aa97726e56cf1c46824b3865f83
AchavalNahuel <achaval353@gmail.com>
2023-12-04 21:50:42 -0300

    Creo tablaTrainers/admin

--- Frontend/src/views/Admin/Admin.jsx
+++ Frontend/src/views/Admin/Admin.jsx
@@ -1,13 +1,22 @@
 import ClientsTable from "../../components/clientsTable/ClientsTable";
+import TrainersTable from "../../components/trainersTable/trainersTable.component";
+import { useNavigate } from "react-router-dom";
 
 const Admin = () => {
+  const navigate = useNavigate();
+  const ejectButton = () => {
+    navigate("/createEj");
+  };
   return (
     <div>
       <div>
-        <button>Crear ejercicios</button>
+        <button onClick={ejectButton}>Crear ejercicios</button>
       </div>
 
       {<ClientsTable />}
+      <div>Tabla de Trainers</div>
+
+      {<TrainersTable />}
     </div>
   );
 };
