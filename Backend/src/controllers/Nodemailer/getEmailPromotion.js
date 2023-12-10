ceceb8ffec3c07ac06f795bbe4615d9cbaf5d062
carloslorenzo7 <carlos__lorenzo@hotmail.com>
2023-12-09 22:25:53 -0300

    cambios

--- Backend/src/controllers/Nodemailer/getEmailPromotion.js
+++ Backend/src/controllers/Nodemailer/getEmailPromotion.js
@@ -4,10 +4,15 @@ const getAllEmails= async(req,res)=>{
    try {
        
        const clients= await Client.findAll({
-           attributes:['email']
+           attributes:['email', 'forename', 'surname']
        });
 
-       const emails= clients.map(client=> client.email)
+       //const emails= clients.map(client=> client.email)
+       const emails= clients.map((client) =>({
+        email: client.email,
+        forename: client.forename,
+        surname: client.surname
+       }))
        return emails;
    } catch (error) {
     throw  new Error(`error al obtener emails de clientes: ${error.message}`)
