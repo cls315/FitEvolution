17bf7ce01d2c4bf00dea6ad78b04bc7eea4ce384
carloslorenzo7 <carlos__lorenzo@hotmail.com>
2023-12-14 16:26:45 -0300

    formRoutines

--- Backend/src/models/Routine.js
+++ Backend/src/models/Routine.js
@@ -31,12 +31,24 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING, // Puedes ajustar el tipo de datos según sea necesario
       allowNull: true, // O false, dependiendo de si el enfoque debe ser obligatorio o no
     },
+    // image: {
+    //   type: DataTypes.TEXT,
+    //   allowNull: true,
+    //   validate: {
+    //     isUrl: {
+    //       msg: "La URL de la imagen no es válida.",
+    //     },
+    //   },
+    // },
     image: {
       type: DataTypes.TEXT,
       allowNull: true,
       validate: {
-        isUrl: {
-          msg: "La URL de la imagen no es válida.",
+        isBlobUrl(value) {
+          // Validar que la URL sea un Blob URL
+          if (!value.startsWith('blob:')) {
+            throw new Error('La URL de la imagen debe ser un Blob URL.');
+          }
         },
       },
     },
