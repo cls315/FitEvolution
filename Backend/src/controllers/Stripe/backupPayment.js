3a4b67513fad678372ddf3a040ccb0123085c605
BGBrianGomez <brianrodri_09@hotmail.com>
2023-12-14 19:38:02 -0300

    Renderizado packs

--- Backend/src/controllers/Stripe/backupPayment.js
+++ Backend/src/controllers/Stripe/backupPayment.js
@@ -1,6 +1,6 @@
 const { Client } = require("../../db");
 
-const backupPayment=async(receipt_email,amountpayment,currency,description,payment_method_types,payment_method)=>{
+const backupPayment=async(receipt_email,amountpayment,currency,description,payment_method_types,payment_method,idTrainer)=>{
     const paymentDate = new Date(); // Convert seconds to milliseconds 
 
 //primero busco el backup antiguo del cliente
@@ -15,13 +15,22 @@ if (existingClient) {
 //agrego los nuevos datos de pago conservando los datos de pagos antiguos
 const newBackups=[paymentDate,receipt_email,amountpayment,currency,description,payment_method_types,payment_method]
 const updatedBackups = [...currentBackups,newBackups];
-console.log( updatedBackups)
+
 //-----------------------------------------------------------------------
     await Client.update(
         { backups: updatedBackups },
         { where: { email: receipt_email } }
       )
 
+}
+if(existingClient){
+    const currentTrainers = existingClient.myTrainers
+    const newTrainers = [...idTrainer];
+    const updateTrainers = [...currentTrainers, ...newTrainers];
+    await Client.update(
+        { myTrainers: updateTrainers},
+        {where: {email: receipt_email}}
+        )
 }
 }
 
--- Backend/src/handlers/PaymentsHndls/paymentsHndls.js
+++ Backend/src/handlers/PaymentsHndls/paymentsHndls.js
@@ -6,8 +6,7 @@ const { Trainer } = require("../../db");
 const { Client } = require("../../db");
 const paymentsHndls = async (req, res) => {
   const { id, amount, idTrainer, userEmail } = req.body;
-  console.log(idTrainer);
-
+console.log("ID TRAINER ------>",idTrainer);
   const stripe = new Stripe(`${SECRET_KEY_STRIPE}`);
   try {
     const payment = await stripe.paymentIntents.create({
@@ -32,37 +31,18 @@ const paymentsHndls = async (req, res) => {
 
     // Guardar recibo de pago en la propiedad backups del modelo cliente
     const client = await Client.findOne({where: {email: userEmail.email }});
-    if (client) {
-      client.dataValues.backups.push({
-        receipt_email,
-        amountpayment,
-        currency,
-        description,
-        payment_method_types,
-        payment_method,
-      });
-      client.dataValues.myTrainers.push({
-        idTrainer
-      });
-      await client.save();
-    }
 
     // Guardar objeto trainer en la propiedad mytrainers del modelo cliente
     for(let i = 0; i < idTrainer.length; i++) {
 
       const trainer = await Trainer.findByPk(idTrainer[i]);
-      console.log(trainer);
       if (trainer) {
-        client.dataValues.myTrainers.push({
-          trainerId: trainer.dataValues.id,
-        });
   
         // Agregar todas las propiedades del cliente al array subscribers del entrenador
         trainer.dataValues.subscribers.push({
           ...client.dataValues// Agrega todas las propiedades del cliente
         });
   
-        await client.save();
         await trainer.save();
       }
     }
@@ -74,7 +54,8 @@ const paymentsHndls = async (req, res) => {
       currency,
       description,
       payment_method_types,
-      payment_method
+      payment_method,
+      idTrainer
     );
 
     res.status(200).send({ message: "compra exitosa" });
