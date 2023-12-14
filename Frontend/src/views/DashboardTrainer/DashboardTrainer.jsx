53c643084128aefbd322580a1e0883f0f784706d
carloslorenzo7 <carlos__lorenzo@hotmail.com>
2023-12-14 13:06:25 -0300

    BotonRutinas y form

--- Frontend/src/views/DashboardTrainer/DashboardTrainer.jsx
+++ Frontend/src/views/DashboardTrainer/DashboardTrainer.jsx
@@ -1,84 +1,79 @@
 //Commons imports
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { URLfrontend } from '../../../configURL';
-import { auth  } from '../../components/firebase/firebase';
-import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { URLfrontend } from "../../../configURL";
+import { auth } from "../../components/firebase/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 import { Link } from "react-router-dom";
 // import { trainerPerfil } from '../../components/redux/actions/actions';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 //components imports
-import MenuprincipalTrainer from '../../components/menuprincipalTainer/menuprincipalTrainer';
-import DashBar from '../../components/Dashbar/Dashbar';
-import PagosprincipalTrainer from '../../components/pagosprincipalTrainer/pagosprincipalTrainer';
-import EntrePrincipalTrainer from '../../components/entreprincipalTRainer/entreprincipalTrainer';
+import MenuprincipalTrainer from "../../components/menuprincipalTainer/menuprincipalTrainer";
+import DashBar from "../../components/Dashbar/Dashbar";
+import PagosprincipalTrainer from "../../components/pagosprincipalTrainer/pagosprincipalTrainer";
+import EntrePrincipalTrainer from "../../components/entreprincipalTRainer/entreprincipalTrainer";
 //styles
-import React from 'react';
-import './DashboardTrainer.css';
-
-
-
-
+import React from "react";
+import "./DashboardTrainer.css";
 
 const DashboardTrainer = (props) => {
+  const [menu, setmenu] = useState("deportes");
+  const allTrainers = useSelector((state) => state.allTrainers);
 
-  const [menu, setmenu] = useState('deportes')
-  const allTrainers=useSelector((state) => state.allTrainers)
-  
-  const trainer=useSelector((state)=>state.trainer)
-  const dispatch=useDispatch()
- 
-  
-  
+  const trainer = useSelector((state) => state.trainer);
+  const dispatch = useDispatch();
+
   //firebase
-  const [userSession, setUserSession] = useState(false)
+  const [userSession, setUserSession] = useState(false);
   //modo escucha de firebase
- useEffect(()=>{
-    onAuthStateChanged(auth, (user)=>{    //esta funcion es de firebase se queda en modo escucha cada vez que se carga la aplicacion.
-    if(user){
-      console.log(user.email)
-      setUserSession(true)
-      dispatch(trainerPerfil(user.email))
-      console.log(trainer)
-      
-    } else{
-      setUserSession(false)
-      
-      console.log(user)
-    }
-    })
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      //esta funcion es de firebase se queda en modo escucha cada vez que se carga la aplicacion.
+      if (user) {
+        console.log(user.email);
+        setUserSession(true);
+        dispatch(trainerPerfil(user.email));
+        console.log(trainer);
+      } else {
+        setUserSession(false);
 
-    
-    return () => {
-    };
-     
+        console.log(user);
+      }
+    });
 
-  },[allTrainers])
+    return () => {};
+  }, [allTrainers]);
 
   //-------------------------*/
   const handleMenu = (e) => {
-    const nom = e.target.name
-    console.log(e.target.name)
-    setmenu(nom)
-  }
-//*manejo de renderizado condicional del boton de admin
+    const nom = e.target.name;
+    console.log(e.target.name);
+    setmenu(nom);
+  };
+  //*manejo de renderizado condicional del boton de admin
 
-  return (<>
-    {
-      userSession ?
-        <div className='bg-trainer-board'>
-          <DashBar handleMenu={handleMenu}/>
-          {menu === "deportes" && <MenuprincipalTrainer trainer={trainer}/>}
+  return (
+    <>
+      {userSession ? (
+        <div className="bg-trainer-board">
+          <DashBar handleMenu={handleMenu} />
+          {menu === "deportes" && <MenuprincipalTrainer trainer={trainer} />}
           {menu === "pagos" && <PagosprincipalTrainer />}
           {menu === "entrenamientos" && <EntrePrincipalTrainer />}
+
           <Link to="/createRoutine">
-          <button>Crear rutina personalizada</button>
-        </Link>
-          <footer className='footerUser'><p>© 2023 FitRevolution </p></footer>
-        </div> :
-       <a href={`${URLfrontend}`}>Su sesion finalizó, haga click aqui.</a>
-    }
-  </>);
+            <button>Crear rutina personalizada</button>
+          </Link>
+
+          <footer className="footerUser">
+            <p>© 2023 FitRevolution </p>
+          </footer>
+        </div>
+      ) : (
+        <a href={`${URLfrontend}`}>Su sesion finalizó, haga click aqui.</a>
+      )}
+    </>
+  );
 };
 
 export default DashboardTrainer;
