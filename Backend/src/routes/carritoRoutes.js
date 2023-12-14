f966846e741651e3380a75b35400e8d53d9cc2a1
AchavalNahuel <achaval353@gmail.com>
2023-12-14 14:47:30 -0300

    ok

--- Backend/src/routes/carritoRoutes.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require("express");
-
-const postCarrito = require("../handlers/CarritoHndls/postCarrito");
-
-const carritoRoutes = Router();
-
-carritoRoutes.post("/", postCarrito);
--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -2,7 +2,6 @@ const { Router } = require("express");
 const trainersRoutes = require("./trainersRoutes");
 const routinesRoutes = require("./routinesRoutes");
 const clientRoutes = require("./clientRoutes");
-const carritoRoutes = require("./carritoRoutes");
 
 const nodemailerRoutes = require("./nodemailerRoutes");
 const exerciseRoutes = require("./exercisesRoutes");
@@ -12,7 +11,6 @@ const router = Router();
 router.use("/trainers", trainersRoutes);
 router.use("/routines", routinesRoutes);
 router.use("/clients", clientRoutes);
-router.use("/carrito", carritoRoutes);
 
 router.use("/nodemailer", nodemailerRoutes);
 router.use("/exercises", exerciseRoutes);
