4ff0e9a8296fd4e3beb1c216694664bf3e9fdec0
carloslorenzo7 <carlos__lorenzo@hotmail.com>
2023-12-15 15:59:31 -0300

    formClinte

--- Backend/src/controllers/Routine/postRotuines.js
+++ Backend/src/controllers/Routine/postRotuines.js
@@ -2,8 +2,6 @@ const { Routine } = require("../../db");
 
 const postRoutines = async (exerc, enfoque, totalDuration, image) => {
   try {
-    // Calcular la duración total sumando las duraciones estimadas de los ejercicios
-
     // Crear la rutina con totalDuration calculado
     const newRoutine = await Routine.create({
       enfoque,
