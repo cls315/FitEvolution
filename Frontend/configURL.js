bc3e1518fbc3bb5ec9a66235ebda1d7fc483addd
FerjuanB <118563752+FerjuanB@users.noreply.github.com>
2023-12-28 16:16:36 -0300

    Update configURL.js

--- Frontend/configURL.js
+++ Frontend/configURL.js
@@ -1,5 +1,5 @@
 //export const URLfrontend = "http://localhost:5173";
 //export const URLSERVER = "http://localhost:3001";
 
-  export const URLfrontend="https://fit-evolution.vercel.app";
-  export const URLSERVER='https://fitevolution.onrender.com';
+  export const URLfrontend="https://fit-evolution-main.vercel.app";
+  export const URLSERVER='https://fitevolution-backend-main.onrender.com';
